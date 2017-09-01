4fcb98807e7048c9194f00f0e2e9d1783123dbce
Thomas Cosby <cosbyts@gmail.com>
2017-09-01 13:47:35 -0700

    (refactor) Moved static login/signup page
      - Components are now React components that will be able to later be integrated into React Router.

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+import Login from './Login';
+import Signup from './Signup';
+
+
+const pages = {
+  '/': <h1>Hello World from React</h1>,
+  '/login': <Login />,
+  '/signup': <Signup />
+};
+
+ReactDOM.render(pages[window.location.pathname], document.getElementById('root'));
