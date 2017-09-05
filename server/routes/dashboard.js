943bc0aeefc3576654318033bb126d4e39ead705
Thomas Cosby <cosbyts@gmail.com>
2017-09-05 13:37:01 -0700

    (refactor) server res.render's sending user information.

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,27 +5,37 @@ const router = express.Router();
 
 router.route('/')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+      user: req.user
+    });
   });
 
 router.route('/edit')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+      user: req.user
+    });
   });
 
 router.route('/schedule')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+      user: req.user
+    });
   });
 
 router.route('/trainers')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+      user: req.user
+    });
   });
 
 router.route('/clients')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+      user: req.user
+    });
   });
 
 module.exports = router;
