f7ea7888d48aae35e4e5cd9810f876c0449adcd3
Thomas Cosby <cosbyts@gmail.com>
2017-09-06 11:53:36 -0700

    (feat) dashboard schedule skeleton

--- client/src/pages/dashboard/schedule/index.js
+++ client/src/pages/dashboard/schedule/index.js
@@ -11,7 +11,62 @@ class Schedule extends React.Component {
 
     return (
 
-      <h1>Schedule View</h1>
+      <div style={{
+        height: '96%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#002957'
+      }}>
+
+        <div id="left-side" style={{
+          height: '100%',
+          width: '25%',
+          minWidth: '200px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#DCD8D7'
+        }}>
+          <span>Control panel</span>
+        </div>
+
+        <div id="right-side" style={{
+          height: '100%',
+          width: '75%',
+          minWidth: '400px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+        }}>
+
+          <div style={{
+            height: '70%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#b6b1af'
+          }}>
+            Schedule Component here
+          </div>
+
+          <div style={{
+            height: '30%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#a9a4a2'
+          }}>
+            Additional Drawer
+          </div>
+
+        </div>
+
+      </div>
 
     );
 
